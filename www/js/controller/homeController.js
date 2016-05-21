ee3235ecd29c5af0ad0b9d44b34ec32fb14b9c4c
okjun kim <ercir@dhcp-v126-182.mobile.uci.edu>
2016-05-20 19:52:58 -0700

    Aatrox added as temporary variable

--- www/js/controller/homeController.js
+++ www/js/controller/homeController.js
@@ -2,6 +2,29 @@ app.controller('homeController', ['$scope', '$state',
 	function($scope, $state) {
 		console.log("Home Controller Loaded");
 
+		$scope.champions = [
+			{
+				id :		1,
+				name:		"aatrox",
+				position:	"top",
+				img: "http://ddragon.leagueoflegends.com/cdn/6.10.1/img/champion/Aatrox.png",
+				price : {
+					rp : 975,
+					ip : 6300
+				},
+				skills : {
+					q : {
+						name : "Dark Flight",
+						desc : "Aatrox takes flight and slams down at a targeted location, dealing damage and knocking up enemies at the center of impact. Aatrox takes flight and slams down at target location, dealing 70/115/160/205/250 (+60% bonus Attack Damage) physical damage and knocking up enemies at the center of impact for 1 second."
+					},
+					e : {
+						name : "Blood Thirst / Blood Price",
+						desc : "While toggled on Aatrox deals bonus damage every third subsequent attack at the expense of his own Health. While toggled off Aatrox restores Health every third subsequent attack. Toggle Off: Blood Thirst: Every third attack, Aatrox restores 20/25/30/35/40 (+25% bonus Attack Damage) Health, increased by 200% when below half Health. Toggle On: Blood Price: Every third attack, Aatrox deals 60/95/130/165/200 (+100% bonus Attack Damage) bonus physical damage and loses Health."
+					}
+				}
+			}
+		];
+
 		$scope.users = [];
 
 		$scope.doSomething = function () {
